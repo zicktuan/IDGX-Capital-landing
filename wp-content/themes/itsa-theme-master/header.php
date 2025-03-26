e81c4285a6509a7cb71ef4da65bf9b9e822ffecb
tuannaBeedu <tuanna@beedu.vn>
2025-03-26 09:55:02 +0700

    update

--- wp-content/themes/itsa-theme-master/header.php
+++ wp-content/themes/itsa-theme-master/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:title" content="<?php bloginfo('name'); ?>">
         <link rel="preload" as="image" href="<?php echo get_template_directory_uri() . '/assets/images/slogan.png'?>">
         <link rel="preload" as="image" href="<?php echo get_template_directory_uri() . '/assets/images/hosted.png'?>">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/assets/css/a5bcef6965bdfcb9.css'?>" data-precedence="next">
